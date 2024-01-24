00063336bf64a275ab8c440aae41f18ee6f6c33b
Bruno César <brunocesar.tenorio1@gmail.com>
2024-01-24 12:22:04 -0300

    adding menu mobile

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 import initNavDetail from "./modules/nav-detail.js";
 import initAccordionNav from "./modules/accordion-nav.js";
 import initChangeImage from "./modules/change-image.js";
+import initMenuMobile from "./modules/menu-mobile.js";
+import initOutsideClick from "./modules/outside-click.js";
 initNavDetail();
 initAccordionNav();
 initChangeImage();
+initMenuMobile();
+initOutsideClick();
 
 if (SimpleAnime) {
   new SimpleAnime();
