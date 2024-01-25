7d917f62717b784106c2c58738d965cb4bd52f08
Bruno César <brunocesar.tenorio1@gmail.com>
2024-01-25 11:38:55 -0300

    updating menu mobile

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -3,14 +3,17 @@ import initOutsideClick from "./outside-click.js";
 export default function initMenuMobile() {
   const button = document.querySelector("[data-menu-mobile-button]");
   const list = document.querySelector("[data-menu-mobile-list]");
-  if (button && list) {
+  const container = document.querySelector("[data-menu-mobile-container]");
+  if (button && list && container) {
     button.addEventListener("click", handleClick);
     function handleClick() {
       button.classList.toggle("active");
       list.classList.toggle("active");
+      container.classList.toggle("active");
       initOutsideClick(list, () => {
         button.classList.remove("active");
         list.classList.remove("active");
+        container.classList.remove("active");
       });
     }
   }
